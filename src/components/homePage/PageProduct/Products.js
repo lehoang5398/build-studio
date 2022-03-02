c487e0a56062f13e64812699a7408b9e2d7db2ad
lehoang5398 <lehoang5398@gmail.com>
2022-03-02 13:03:50 +0700

    xu ly vong lap

--- src/components/homePage/PageProduct/Products.js
+++ src/components/homePage/PageProduct/Products.js
@@ -1,213 +1,90 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Product,TitleProduct } from '../../../assets/image';
+import { Product, TitleProduct } from '../../../assets/image';
 function Products(props) {
+  const product = [
+    {
+      id:1,
+      imageTitle: TitleProduct,
+      TileText: 'BUILD AN APP LIKE',
+      ProductName: '3D Look',
+      imageProduct: Product,
+      titleBody: 'INCLUDED FEATURES',
+      blogBody: 'activity log, Admin Console , Account Creation',
+      features: '+ 34 other features',
+      from: 'from',
+      price: '$ 27',
+      platForm: 'platfrom',
+      ber: 'ber'
+    },
+    {
+      id:2,
+      imageTitle: TitleProduct,
+      TileText: 'BUILD AN APP LIKE',
+      ProductName: '3D Look',
+      imageProduct: Product,
+      titleBody: 'INCLUDED FEATURES',
+      blogBody: 'activity log, Admin Console , Account Creation',
+      features: '+ 34 other features',
+      from: 'from',
+      price: '$ 27',
+      platForm: 'platfrom',
+      ber: 'ber'
+    },
+    {
+      id:3,
+      imageTitle: TitleProduct,
+      TileText: 'BUILD AN APP LIKE',
+      ProductName: '3D Look',
+      imageProduct: Product,
+      titleBody: 'INCLUDED FEATURES',
+      blogBody: 'activity log, Admin Console , Account Creation',
+      features: '+ 34 other features',
+      from: 'from',
+      price: '$ 27',
+      platForm: 'platfrom',
+      ber: 'ber'
+    },
+
+  ]
   return (
     <ul className="product-list">
-      <li className="product-item">
-        <div className="templateCard">
-          <div className="product-header">
-            <img className="product-img" alt ="" src={TitleProduct} />
-            <div className="title-text">
-              BUILD AN APP LIKE
-              <span>3D Look</span>
-            </div>
-            <div className="actionbx">
-              <input className="morebtn" type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
-            </div>
-          </div>
-          <div className="product-body">
-            <img className="product-body-img" alt ="" src={Product} />
-            <div className="product-featureDetail">
-              <span className="body-title">INCLUDED FEATURES</span>
-              <span className="body-blog">activity log, Admin Console , Account Creation</span>
-              <span className="features">+ 34 other features</span>
-            </div>
-          </div>
-          <div className="product-footer">
-            <div className="title-price">
-              <span className="from">From</span>
-              <div className="pricewithmonth">
-                <span className="price">$ 27</span>
-                <span className="platForm">ber<br /> platfrom </span>
+      {product.map((item) => (
+        <li key ={item?.id} className="product-item">
+          <div className="templateCard">
+            <div className="product-header">
+              <img className="product-img" alt="" src={item?.imageTitle} />
+              <div className="title-text">
+                {item?.TileText}
+                <span>{item?.ProductName}</span>
+              </div>
+              <div className="actionbx">
+                <input className="morebtn" type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
               </div>
             </div>
-            <div className="detail">
-              <Link to ='/' className="viewDetail">View Details</Link>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li className="product-item">
-        <div className="templateCard">
-          <div className="product-header">
-            <img className="product-img" alt ="" src={TitleProduct} />
-            <div className="title-text">
-              BUILD AN APP LIKE
-              <span>3D Look</span>
-            </div>
-            <div className="actionbx">
-              <input className="morebtn" type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
-            </div>
-          </div>
-          <div className="product-body">
-            <img className="product-body-img" alt ="" src={Product} />
-            <div className="product-featureDetail">
-              <span className="body-title">INCLUDED FEATURES</span>
-              <span className="body-blog">activity log, Admin Console , Account Creation</span>
-              <span className="features">+ 34 other features</span>
-            </div>
-          </div>
-          <div className="product-footer">
-            <div className="title-price">
-              <span className="from">From</span>
-              <div className="pricewithmonth">
-                <span className="price">$ 27</span>
-                <span className="platForm">ber<br /> platfrom </span>
+            <div className="product-body">
+              <img className="product-body-img" alt="" src={item?.imageProduct} />
+              <div className="product-featureDetail">
+                <span className="body-title">{item?.titleBody}</span>
+                <span className="body-blog">{item?.blogBody}</span>
+                <span className="features">{item?.features}</span>
               </div>
             </div>
-            <div className="detail">
-              <Link to ='/' className="viewDetail">View Details</Link>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li className="product-item">
-        <div className="templateCard">
-          <div className="product-header">
-            <img className="product-img" alt ="" src={TitleProduct} />
-            <div className="title-text">
-              BUILD AN APP LIKE
-              <span>3D Look</span>
-            </div>
-            <div className="actionbx">
-              <input className="morebtn" type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
-            </div>
-          </div>
-          <div className="product-body">
-            <img className="product-body-img" alt ="" src={Product} />
-            <div className="product-featureDetail">
-              <span className="body-title">INCLUDED FEATURES</span>
-              <span className="body-blog">activity log, Admin Console , Account Creation</span>
-              <span className="features">+ 34 other features</span>
-            </div>
-          </div>
-          <div className="product-footer">
-            <div className="title-price">
-              <span className="from">From</span>
-              <div className="pricewithmonth">
-                <span className="price">$ 27</span>
-                <span className="platForm">ber<br /> platfrom </span>
+            <div className="product-footer">
+              <div className="title-price">
+                <span className="from">{item?.features}</span>
+                <div className="pricewithmonth">
+                  <span className="price">{item?.price}</span>
+                  <span className="platForm">{item?.ber}<br /> {item.platForm} </span>
+                </div>
+              </div>
+              <div className="detail">
+                <button className="viewDetail">View Details</button>
               </div>
             </div>
-            <div className="detail">
-              <Link to ='/' className="viewDetail">View Details</Link>
-            </div>
           </div>
-        </div>
-      </li>
-      <li className="product-item">
-        <div className="templateCard">
-          <div className="product-header">
-            <img className="product-img" alt ="" src={TitleProduct} />
-            <div className="title-text">
-              BUILD AN APP LIKE
-              <span>3D Look</span>
-            </div>
-            <div className="actionbx">
-              <input className="morebtn" type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
-            </div>
-          </div>
-          <div className="product-body">
-            <img className="product-body-img" alt ="" src={Product} />
-            <div className="product-featureDetail">
-              <span className="body-title">INCLUDED FEATURES</span>
-              <span className="body-blog">activity log, Admin Console , Account Creation</span>
-              <span className="features">+ 34 other features</span>
-            </div>
-          </div>
-          <div className="product-footer">
-            <div className="title-price">
-              <span className="from">From</span>
-              <div className="pricewithmonth">
-                <span className="price">$ 27</span>
-                <span className="platForm">ber<br /> platfrom </span>
-              </div>
-            </div>
-            <div className="detail">
-              <Link to ='/' className="viewDetail">View Details</Link>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li className="product-item">
-        <div className="templateCard">
-          <div className="product-header">
-            <img className="product-img" alt ="" src={TitleProduct} />
-            <div className="title-text">
-              BUILD AN APP LIKE
-              <span>3D Look</span>
-            </div>
-            <div className="actionbx">
-              <input className="morebtn" type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
-            </div>
-          </div>
-          <div className="product-body">
-            <img className="product-body-img" alt ="" src={Product} />
-            <div className="product-featureDetail">
-              <span className="body-title">INCLUDED FEATURES</span>
-              <span className="body-blog">activity log, Admin Console , Account Creation</span>
-              <span className="features">+ 34 other features</span>
-            </div>
-          </div>
-          <div className="product-footer">
-            <div className="title-price">
-              <span className="from">From</span>
-              <div className="pricewithmonth">
-                <span className="price">$ 27</span>
-                <span className="platForm">ber<br /> platfrom </span>
-              </div>
-            </div>
-            <div className="detail">
-              <Link to ='/' className="viewDetail">View Details</Link>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li className="product-item">
-        <div className="templateCard">
-          <div className="product-header">
-            <img className="product-img" alt ="" src={TitleProduct} />
-            <div className="title-text">
-              BUILD AN APP LIKE
-              <span>3D Look</span>
-            </div>
-            <div className="actionbx">
-              <input className="morebtn" type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
-            </div>
-          </div>
-          <div className="product-body">
-            <img className="product-body-img" alt ="" src={Product} />
-            <div className="product-featureDetail">
-              <span className="body-title">INCLUDED FEATURES</span>
-              <span className="body-blog">activity log, Admin Console , Account Creation</span>
-              <span className="features">+ 34 other features</span>
-            </div>
-          </div>
-          <div className="product-footer">
-            <div className="title-price">
-              <span className="from">From</span>
-              <div className="pricewithmonth">
-                <span className="price">$ 27</span>
-                <span className="platForm">ber<br /> platfrom </span>
-              </div>
-            </div>
-            <div className="detail">
-              <Link to ='/' className="viewDetail">View Details</Link>
-            </div>
-          </div>
-        </div>
-      </li>
+        </li>
+      ))}
+
     </ul>
   );
 }
